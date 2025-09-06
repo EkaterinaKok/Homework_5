7426d8099abe488fc3430e9e8fb7a5f3856bca54
User-PC <2356532@mail.ru>
2025-09-06 11:11:14 +0500

    ДЗ поправила

--- src/Main.java
+++ src/Main.java
@@ -5,48 +5,44 @@ public class Main {
 
         //Task 1
         System.out.println("Задача 1");
-        byte iOs = 0;
-        byte android = 1;
         byte clientOs = 0;
-        if (clientOs == iOs) {
+        if (clientOs == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке.");
-        } else if (clientOs == android) {
+        } else if (clientOs == 1) {
             System.out.println("Установите версию приложения для Android по ссылке.");
         }
         System.out.println();
 
         //Task 2
         System.out.println("Задача 2");
-        byte iOs2 = 0;
-        byte android2 = 1;
-        byte clientOs2 = 0;
+        String operationSystem;
+        byte clientOS = 1;
         short yearVersion = 2015;
-        short clientDeviceYear = 2020;
-        if (clientOs2 == iOs2 && clientDeviceYear < yearVersion) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
-        } else if (clientOs2 == iOs2 && clientDeviceYear >= yearVersion) {
-            System.out.println("Установите версию приложения для iOS по ссылке.");
+        short clientDeviceYear = 2018;
+        if (clientOS == 0) {
+            operationSystem = "iOS";
+        } else {
+            operationSystem = "Android";
         }
-        if (clientOs2 == android2 && clientDeviceYear < yearVersion) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке.");
-        } else if (clientOs2 == android2 && clientDeviceYear >= yearVersion) {
-            System.out.println("Установите версию приложения для Android по ссылке.");
+        if (clientDeviceYear < yearVersion) {
+            System.out.println("Установите облегченную версию приложения для " + operationSystem + " по ссылке.");
+        } else {
+            System.out.println("Установите версию приложения для " + operationSystem + " по ссылке.");
         }
         System.out.println();
 
         //Task 3
         System.out.println("Задача 3");
         int firstYearLeap = 1584;
-        int year3 = 2021;
-        if (year3 >= firstYearLeap && year3 % 100 == 0 && year3 % 400 == 0) {
-            System.out.println("Год " + year3 + " является високосным.");
-        } else if (year3 >= firstYearLeap && year3 % 4 == 0) {
-            System.out.println("Год " + year3 + " является високосным.");
+        int year = 2021;
+        if (year >= firstYearLeap) {
+            if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+                System.out.println("Год " + year + " является високосным.");
+            } else {
+                System.out.println("Год " + year + " не является високосным.");
+            }
         } else {
-            System.out.println("Год " + year3 + " не является високосным.");
-        }
-        if (year3 < firstYearLeap) {
-            System.out.println("Год " + year3 + " меньше 1584 года, в котором ввели високосный год.");
+            System.out.println("Год " + year + " должен быть больше или равен 1584 г.");
         }
         System.out.println();
 
@@ -54,12 +50,12 @@ public class Main {
         System.out.println("Задача 4");
         short deliveryDistance = 95;
         short deliveryTime = 1;
-        if (deliveryDistance < 20) {
+        if (deliveryDistance <= 20) {
             System.out.println("Потребуется дней: " + deliveryTime);
-        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+        } else if (deliveryDistance <= 60) {
             deliveryTime += 1;
             System.out.println("Потребуется дней: " + deliveryTime);
-        } else if ((deliveryDistance >= 60 && deliveryDistance < 100)) {
+        } else if (deliveryDistance <= 100) {
             deliveryTime += 2;
             System.out.println("Потребуется дней: " + deliveryTime);
         } else {
@@ -87,5 +83,4 @@ public class Main {
                 System.out.println(monthNumber + "-ого месяца не существует.");
         }
     }
-
 }
